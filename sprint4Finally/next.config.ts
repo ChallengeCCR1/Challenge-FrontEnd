0c5ce688b40b433a70d4ab219fa3f17a781b313c
ChallengeOne-MAT <rm561090@fiap.com.br>
2025-05-20 01:55:21 -0300

    Atualiza next.config.ts

--- sprint4Finally/next.config.ts
+++ sprint4Finally/next.config.ts
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
   },
   async rewrites() {
     return [
@@ -13,4 +17,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
